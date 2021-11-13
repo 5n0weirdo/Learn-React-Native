6885e6a1eebb32aefacafbd27117c992d2c04c98
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 10:51:59 +0200

    justifyContent and flexDirection

--- App.js
+++ App.js
@@ -4,10 +4,21 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "purple", flex: 2 }}></View>
-      <View style={{ backgroundColor: "black", flex: 1 }}></View>
-      <View style={{ backgroundColor: "blue", flex: 1 }}></View>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{ backgroundColor: "purple", width: 100, height: 100 }}
+      ></View>
+      <View
+        style={{ backgroundColor: "black", width: 100, height: 100 }}
+      ></View>
+      <View style={{ backgroundColor: "blue", width: 100, height: 100 }}></View>
     </View>
   );
 }
