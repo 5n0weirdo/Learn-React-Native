28e39bfb8f7459a055d5fefb709daec0ac7b5b3c
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 11:24:58 +0200

    Exercise: Added background image

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{ backgroundColor: "black", width: 100, height: 100 }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          width: 100,
-          height: 100,
-          position: "absolute",
-          right: 20,
-          top: 20,
+    <View>
+      <Image
+        source={{
+          with: "100%",
+          height: "100%",
+          uri: "https://photoshop-kopona.com/uploads/posts/2018-08/1535575878_1.jpg",
         }}
-      ></View>
-      <View
-        style={{ backgroundColor: "green", width: 100, height: 100 }}
-      ></View>
+      />
     </View>
   );
 }
