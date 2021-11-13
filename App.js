37ebd60124f695c9e34df572b88787cf093aa8f8
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 11:12:04 +0200

    Positioning

--- App.js
+++ App.js
@@ -14,18 +14,18 @@ export default function App() {
       }}
     >
       <View
-        style={{
-          backgroundColor: "purple",
-          flexGrow: 1, //flex:1
-          flexShrink: -1, //flex:-1
-          width: 400,
-          height: 100,
-        }}
-      ></View>
-      <View
         style={{ backgroundColor: "black", width: 100, height: 100 }}
       ></View>
-      <View style={{ backgroundColor: "blue", width: 100, height: 100 }}></View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          position: "absolute",
+          right: 20,
+          top: 20,
+        }}
+      ></View>
       <View
         style={{ backgroundColor: "green", width: 100, height: 100 }}
       ></View>
