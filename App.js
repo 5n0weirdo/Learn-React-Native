1918a29848cdc2835f719f8c458000fd102a541a
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 10:41:05 +0200

    Set flexbox color

--- App.js
+++ App.js
@@ -1,42 +1,7 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  return (
-    <SafeAreaView style={[styles.container]}>
-      <View
-        style={{
-          backgroundColor: "blue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
-  );
+  return <View style={{ backgroundColor: "purple", flex: 1 }}></View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === StatusBar.currentHeight ? 20 : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
