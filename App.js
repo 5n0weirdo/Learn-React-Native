9444875167c256373e5436e7a0b347ba373bc020
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 15:25:45 +0200

    Exercise: Display background image

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, SafeAreaView, Button } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <Image
-        source={{
-          with: "100%",
-          height: "75%",
-          uri: "https://photoshop-kopona.com/uploads/posts/2018-08/1535575878_1.jpg",
-        }}
-      />
-      <View style={{ backgroundColor: "black", flex: 1 }}></View>
-      <View style={{ backgroundColor: "blue", flex: 1 }}></View>
-    </View>
-  );
+  return <WelcomeScreen></WelcomeScreen>;
 }
