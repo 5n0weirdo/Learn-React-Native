0b1e32b5882a5208a7e2ffedc27d3d19865634b0
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 08:07:08 +0200

    onPress and numberOfLines

--- App.js
+++ App.js
@@ -3,9 +3,15 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Pressed");
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore{" "}
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -15,5 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
