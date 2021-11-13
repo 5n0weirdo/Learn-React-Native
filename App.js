7848b07f62aa10a7d454b9ad4795e0ffee68b7f7
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 11:06:59 +0200

    flexGrow and flexShrink

--- App.js
+++ App.js
@@ -11,14 +11,14 @@ export default function App() {
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "purple",
-          width: 100,
+          flexGrow: 1, //flex:1
+          flexShrink: -1, //flex:-1
+          width: 400,
           height: 100,
         }}
       ></View>
@@ -29,7 +29,6 @@ export default function App() {
       <View
         style={{ backgroundColor: "green", width: 100, height: 100 }}
       ></View>
-      <View style={{ backgroundColor: "grey", width: 100, height: 100 }}></View>
     </View>
   );
 }
