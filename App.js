bf151375e7297a4bbfcaefea877f97d1fa9daf22
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 10:45:17 +0200

    Multiple flex conatiner

--- App.js
+++ App.js
@@ -3,5 +3,11 @@ import React from "react";
 import { View } from "react-native";
 
 export default function App() {
-  return <View style={{ backgroundColor: "purple", flex: 1 }}></View>;
+  return (
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View style={{ backgroundColor: "purple", flex: 2 }}></View>
+      <View style={{ backgroundColor: "black", flex: 1 }}></View>
+      <View style={{ backgroundColor: "blue", flex: 1 }}></View>
+    </View>
+  );
 }
