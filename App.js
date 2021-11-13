ad1fd9b7b5ece7340ee9da396caab9ab31c20641
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 08:34:19 +0200

    Added button with title

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-} from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Press");
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
-      <Image source={require("./assets/favicon.png")} />
-      <TouchableHighlight onPress={handlePress}>
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-
+      <Button title="Click me" />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
