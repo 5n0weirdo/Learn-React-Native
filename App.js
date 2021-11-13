27182dee2a7f68f85833c82e952ef945df4ee45a
5n0weirdo <snoweirdo@protonmail.com>
2021-11-13 11:47:27 +0200

    Exercise: Added image and containers

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
       <Image
         source={{
           with: "100%",
-          height: "100%",
+          height: "75%",
           uri: "https://photoshop-kopona.com/uploads/posts/2018-08/1535575878_1.jpg",
         }}
       />
+      <View style={{ backgroundColor: "black", flex: 1 }}></View>
+      <View style={{ backgroundColor: "blue", flex: 1 }}></View>
     </View>
   );
 }
